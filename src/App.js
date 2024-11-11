769eedc53e1d07e255079dca73086d10ce19a8df
Gundumi Sri Krishna Sudhindra <srikrishnasudheendra@gmail.com>
2024-11-12 00:06:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="app-container">
       <h1 className="app-title">Student Management System</h1>
+      <p className="developer-info">Developed by Gundumi Sri Krishna Sudhindra</p>
       <StudentList />
     </div>
   );
